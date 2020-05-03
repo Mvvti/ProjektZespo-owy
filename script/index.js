f7777582e62bf28e1bf6e4e7493f938d4520d1bd
Mateusz Orzeszyna <orzeszynam@gmail.com>
2020-05-03 14:17:06 +0200

    dodanie projektu zespolowego

---
+++ script/index.js
@@ -0,0 +1,10 @@
+firebase.initializeApp({
+  apiKey: "AIzaSyAmFeFJlItIa2ijvvPGnGx1hcaUB5cbJ0Q",
+    authDomain: "projektzespolowy-9ca66.firebaseapp.com",
+    databaseURL: "https://projektzespolowy-9ca66.firebaseio.com",
+    projectId: "projektzespolowy-9ca66",
+    storageBucket: "projektzespolowy-9ca66.appspot.com",
+    messagingSenderId: "875247823861",
+    appId: "1:875247823861:web:a9130cee1576e1f0ac0ebc",
+    measurementId: "G-1MG42ZPBPR"
+});
